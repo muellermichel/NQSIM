3635a4ba11c99e93a5812be8e82ac5940aab5e95
Michel Müller <mmueller.ee@gmail.com>
2018-11-06 13:36:51 +0100

    added ~20% improvement by letting links sleep, more config options in order to simplify validation process

--- java/Run.sh
+++ java/Run.sh
@@ -13,6 +13,21 @@ if [ "$2" != "" ]; then
 	nagents="$2"
 fi
 
+verbose="false"
+if [ "$3" != "" ]; then
+	verbose="$3"
+fi
+
+simtime="3600"
+if [ "$4" != "" ]; then
+	simtime="$4"
+fi
+
+min_plan_length="200"
+if [ "$5" != "" ]; then
+	min_plan_length="$5"
+fi
+
 hosts_option=""
 if [ "$HOSTS" != "" ] && [ -f $HOSTS ]; then
    echo "using ${HOSTS}:"
@@ -33,7 +48,7 @@ mpirun \
 		-Xmx10G \
 		-enableassertions \
 		-classpath "${script_dir}/target/test-classes:${script_dir}/target/classes:${HOME}/.m2/repository/com/fasterxml/jackson/core/jackson-databind/2.9.5/jackson-databind-2.9.5.jar:${HOME}/.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.9.5/jackson-annotations-2.9.5.jar:${HOME}/.m2/repository/com/fasterxml/jackson/core/jackson-core/2.9.5/jackson-core-2.9.5.jar:${HOME}/.m2/repository/org/junit/jupiter/junit-jupiter-api/5.3.1/junit-jupiter-api-5.3.1.jar:${HOME}/.m2/repository/org/apiguardian/apiguardian-api/1.0.0/apiguardian-api-1.0.0.jar:${HOME}/.m2/repository/org/opentest4j/opentest4j/1.1.1/opentest4j-1.1.1.jar:${HOME}/.m2/repository/org/junit/platform/junit-platform-commons/1.3.1/junit-platform-commons-1.3.1.jar:${HOME}/.m2/repository/fastutil/fastutil/5.0.9/fastutil-5.0.9.jar:${HOME}/.m2/repository/io/protostuff/protostuff-core/1.5.9/protostuff-core-1.5.9.jar:${HOME}/.m2/repository/io/protostuff/protostuff-api/1.5.9/protostuff-api-1.5.9.jar:${HOME}/.m2/repository/io/protostuff/protostuff-runtime/1.5.9/protostuff-runtime-1.5.9.jar:${HOME}/.m2/repository/io/protostuff/protostuff-collectionschema/1.5.9/protostuff-collectionschema-1.5.9.jar:${HOME}/.m2/repository/commons-io/commons-io/1.3.2/commons-io-1.3.2.jar:${mpi_jar_path}" \
-		ch.ethz.systems.nqsim.ChineseCityTest ${nagents} \
+		ch.ethz.systems.nqsim.ChineseCityTest ${nagents} ${verbose} ${simtime} ${min_plan_length} \
 	| tee output_${timestamp}_np${np}.txt \
 	&& :
 rv=$?
